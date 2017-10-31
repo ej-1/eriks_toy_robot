9ad7293ee17c5084dd9ba2e62d664d3aa973d73c
Erik <erikwjonsson@gmail.com>
2017-10-31 17:46:19 +0100

    Added a way to exit command line loop.

--- main.rb
+++ main.rb
@@ -1,14 +1,11 @@
 require_relative 'lib/toy_robot'
 require_relative 'lib/command_runner'
 
-require 'pry'
-
-
 @command_runner = CommandRunner.new
 @toy_robot = ToyRobot.new
 
 loop do
 	command_line_input = gets.chomp
 	@command_runner.run_command(command_line_input, @toy_robot)
-	#break if command_line_input == 'exit'
+	break if command_line_input == 'exit'
 end
